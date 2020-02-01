a1ab92e538291fdba419754194cdfb9de1a40910
gf <gf@at.com>
2020-02-01 23:29:13 +0800

    day2/1（2）

--- cc/src/ａａ/ａａ.java
+++ cc/src/ａａ/ａａ.java
@@ -11,5 +11,7 @@ public class ａａ {
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
         System.out.println("aaaaaaaaaaa");
+        //这是我第一次提交git
     }
+
 }
